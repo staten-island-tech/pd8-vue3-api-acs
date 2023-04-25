62839f83cc7b751ffbb7ea1f0c226c42b9c6a7f4
saharvitale <113132357+saharvitale@users.noreply.github.com>
2023-04-25 13:51:42 -0400

    vid1 9:35

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,9 +1,21 @@
+<template>
+  <div></div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+const pokemon = ref('')
+//ref creates a variable thats going to change v model (pokemon api)
+
+async function getPokemon() {
+  let res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+  let data = await res.json()
+  pokemon.value = data.results
+}
+onMounted(() => {
+  getPokemon()
+})
+//mounted is a stage of the vue component's life cycle
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
